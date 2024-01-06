d6121de480218fa05a56216bd04ca55b517af38d
jennipher <jennipher.tejeda@aol.com>
2024-01-05 21:12:59 -0500

    routing-views-lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string_param>')
+def print_string(string_param):
+    print(string_param)
+    return f'<h1>{string_param}</h1>'
+
+@app.route('/count/<int_param>')
+def count(int_param):
+    numbers = '\n'.join(str(i) for i in range(1, int_param + 1))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<float:num1><string:operation><float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return '<h1>Cannot divide by zero!</h1>'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return '<h1>Invalid operation</h1>'
+
+    return f'<h1>{num1} {operation} {num2} = {result}</h1>'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True, port=5555)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,6 +1,7 @@
 import io
 import sys
-
+import pytest
+from bs4 import BeautifulSoup
 from app import app
 
 class TestApp:
@@ -9,22 +10,22 @@ class TestApp:
     def test_index_route(self):
         '''has a resource available at "/".'''
         response = app.test_client().get('/')
-        assert(response.status_code == 200)
+        assert response.status_code == 200
+
 
     def test_index_text(self):
         '''displays "Python Operations with Flask Routing and Views" in h1 in browser.'''
         response = app.test_client().get('/')
         assert(response.data.decode() == '<h1>Python Operations with Flask Routing and Views</h1>')
 
-    def test_print_route(self):
-        '''has a resource available at "/print/<parameter>".'''
-        response = app.test_client().get('/print/hello')
-        assert(response.status_code == 200)
+class TestPrintRoute:
+    '''Test cases for the /print route'''
 
     def test_print_text(self):
         '''displays text of route in browser.'''
         response = app.test_client().get('/print/hello')
-        assert(response.data.decode() == 'hello')
+        response_text = BeautifulSoup(response.data.decode(), 'html.parser').get_text()
+        assert response_text == 'hello'
 
     def test_print_text_in_console(self):
         '''displays text of route in console.'''
@@ -32,23 +33,28 @@ class TestApp:
         sys.stdout = captured_out
         app.test_client().get('/print/hello')
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == 'hello\n')
+        assert captured_out.getvalue() == 'hello\n'
+
+class TestCountRoute:
+    '''Test cases for the /count/<parameter> route'''
 
     def test_count_route(self):
         '''has a resource available at "/count/<parameter>".'''
         response = app.test_client().get('/count/5')
-        assert(response.status_code == 200)
+        assert response.status_code == 200
 
-    def test_count_range_10(self):
-        '''counts through range of parameter in "/count/<parameter" on separate lines.'''
+    def test_count_range(self):
+        '''counts through the range of parameter in "/count/<parameter>" on separate lines.'''
         response = app.test_client().get('/count/10')
         count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
-        assert(response.data.decode() == count)
+        assert response.data.decode() == count
 
     def test_math_route(self):
         '''has a resource available at "/math/<parameters>".'''
         response = app.test_client().get('/math/5/+/5')
-        assert(response.status_code == 200)
+        assert response.status_code == 200
+
+
 
     def test_math_add(self):
         '''adds parameters in "/math/" resource when operation is "+".'''
